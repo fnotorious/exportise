9e775716e09431225ea7e14857795ab8e5d47e28
fnotorious <fnato001@fiu.edu>
2023-01-07 03:22:21 -0500

    Second commit

--- app/src/components/front-page/front-page.js
+++ app/src/components/front-page/front-page.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import styles from './front-page.module.css'
+import banner from '../../assets/homepage-design-CHRISTMAS-PNG.png'
 
 export default function FrontPage() {
   return (
     <div className={styles.canvas}>
-        <div className={styles.mainSection}></div>
+        <div className={styles.mainSection}>
+            <div className={styles.centerBox}>
+                <img src={banner} alt="BANNER" className={styles.logoBanner}></img>
+                <div className={styles.logoText}>Comparing trade and economy between nations</div>
+            </div>
+            <div className={styles.textBox}></div>
+        </div>
     </div>
   )
 }
