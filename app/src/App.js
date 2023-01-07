6bae1a0f41ea557e833385839338fc9c4ec2c03e
fnotorious <fnato001@fiu.edu>
2023-01-07 01:51:02 -0500

    Getting started

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import FrontPage from './components/front-page/front-page.js'
+
+
+function App() {
+  return (
+    <div className="App">
+      <FrontPage></FrontPage>
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/components/front-page/front-page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './front-page.module.css'
+
+export default function FrontPage() {
+  return (
+    <div className={styles.canvas}>
+        <div className={styles.mainSection}></div>
+    </div>
+  )
+}
