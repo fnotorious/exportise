56e33969d6446bd77c461aca18b9ff0d6eafb556
fnotorious <fnato001@fiu.edu>
2023-02-05 00:52:35 -0500

    Error with commits

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,13 +1,31 @@
 import './App.css';
-import FrontPage from './components/front-page/front-page.js'
-
+import { useEffect , useState , useCallback } from 'react';
+import { useLocation , Route , Routes } from 'react-router-dom';
+import FrontPage from './components/pages/front-page.js'
+import LoadingScreen from './components/pages/loading-screen';
 
 function App() {
+  const location = useLocation();
+  const [currentPage, setCurrentPage] = useState('home');
+  const [darkMode, setDarkMode] = useState(false);
+
+  const handleChange = useCallback((newValue) => {
+    setDarkMode(newValue);
+  }, []);
+
+  useEffect(() => {
+    const page = location.pathname.substr(1);
+    setCurrentPage(page);
+  }, [location]);
+
   return (
-    <div className="App">
-      <FrontPage></FrontPage>
+    <div className='App'>
+      <Routes>
+        <Route path="/" element={<FrontPage currentPage={currentPage} handleChange={handleChange} darkMode={darkMode} />} />
+        <Route path="/loading" element={<LoadingScreen currentPage={currentPage} darkMode={darkMode} />} />
+      </Routes>
     </div>
-  );
+  )
 }
 
 export default App;
