e944f85f49295d9a3245ca348b20445127be5067
fnotorious <fnato001@fiu.edu>
2023-03-16 05:25:34 -0400

    Error prevention

--- app/src/components/banner/banner.js
+++ app/src/components/banner/banner.js
@@ -10,10 +10,10 @@ export const Banner = React.memo((props) => {
             {props.showLoading === false ? <div className={styles.shadow}></div> : ''}
             <div className={styles.names}>
                 <div className={styles.nameDisplay}>
-                    {props.dataLoaded === false || props.isOnline === false ? <div className={styles.nameLoader}></div> : <p className={styles.name}>{props.data[props.countryNum].name.common}</p>}
+                    {(!(props.data && props.data.length > 1) && !props.dataLoaded) || props.isOnline === false ? <div className={styles.nameLoader}></div> : <p className={styles.name}>{props.data[props.countryNum].name.common}</p>}
                 </div>
                 <div className={styles.nativeNameDisplay}>
-                    {props.dataLoaded === false || props.isOnline === false ? <div className={styles.nativeNameLoader}></div> : <p className={styles.nativeName}>{props.country === 'il'
+                    {(!(props.data && props.data.length > 1) && !props.dataLoaded) || props.isOnline === false ? <div className={styles.nativeNameLoader}></div> : <p className={styles.nativeName}>{props.country === 'il'
                     ? props.data[props.countryNum].name.nativeName[Object.keys(props.data[props.countryNum].name.nativeName)[1]].common
                     : props.data[props.countryNum].name.nativeName[Object.keys(props.data[props.countryNum].name.nativeName)[0]].common}</p>}
                 </div>
